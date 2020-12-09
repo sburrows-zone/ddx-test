81c9f612e9427fa91685248fa79a01044e80728c
Stuart Burrows <42245091+sburrows-zone@users.noreply.github.com>
2020-12-09 15:36:28 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>
+        Hi Ray!{" "}
+        <span role="img" aria-label="hand waving">
+          👋
+        </span>
+      </h1>
+    </div>
+  );
+}
